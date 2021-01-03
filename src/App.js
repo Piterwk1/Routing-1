6f2bd6056e6f99b5259f47e4d34322d78a9abef1
Piotr Kaminski <piterwk1@.wp.pl>
2021-01-03 11:17:00 +0100

    app complited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {BrowserRouter, Link, NavLink, Route, Switch} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Home = () => <h1>Strona startowa</h1>
+const News = () => <h1>Aktualnosci</h1>
+const Contact = () => <h1>Kontakt do nas</h1>
+const ErrorPage = () => <h1>Strona nie istnieje</h1>
+class App extends Component {
+  
+  render() { 
+    return ( 
+      <BrowserRouter>
+      <div className="App">
+        <header>
+          <nav>
+            <ul>
+              <li><NavLink to="/" exact activeClassName='home_selected'>Start</NavLink></li>
+              <li><NavLink to="/news" activeClassName='news_selected'>Aktualnosci</NavLink></li>
+              <li><NavLink to="/contact" activeClassName='contact_selected'>Kontakt</NavLink></li>
+            </ul>
+          </nav>
+        </header>
+        <section>
+          <Switch>
+          <Route path='/' exact component={Home}/>
+          <Route path='/news' component={News}/>
+          <Route path='/contact' component={Contact}/>
+          <Route  component={ErrorPage}/>
+          </Switch>
+        </section>
+      </div>
+      </BrowserRouter>
+     );
+  }
 }
-
+ 
 export default App;
